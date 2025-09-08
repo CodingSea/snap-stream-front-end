1b448097d7e8b7db2a127b574eb70bb05550f84f
FahadQ <Fhdq2003@email.com>
2025-09-08 16:17:05 +0300

    completed the search bar

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -4,13 +4,20 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import "../../Main.css"
 import PostDetails from '../PostDetails/PostDetails'
-import { getPosts } from '../../../lib/postAPI'
+import { getPosts, searchPosts } from '../../../lib/postAPI'
 import { RingLoader } from 'react-spinners'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function SearchPage()
 {
     const navigate = useNavigate()
     const [posts, setPosts] = useState([])
+    const [formData, setFormData] = useState(
+        {
+            searchText: ""
+        }
+    );
 
     async function listPosts()
     {
@@ -30,6 +37,21 @@ function SearchPage()
         navigate(`/search/post/${ index }`)
     }
 
+    async function handleSearch(event)
+    {
+        event.preventDefault();
+
+        try
+        {
+            const postsList = await searchPosts(formData.searchText);
+            setPosts(postsList.data);
+        }
+        catch (error)
+        {
+            console.log(error);
+        }
+    }
+
     useEffect(() =>
     {
         listPosts();
@@ -38,20 +60,27 @@ function SearchPage()
     return (
         <>
             <h1>Search Page</h1>
+
+            <form onSubmit={ handleSearch }>
+                <input type="text" name="searchText" onChange={ (event) => setFormData({ ...formData, searchText: event.target.value }) } value={ formData.searchText } />
+
+                <button type='submit'><FontAwesomeIcon icon={ faSearch } /></button>
+            </form>
+
             <button onClick={ () => { navigate('/post/new') } }>+</button>
 
             <div className='posts-container'>
                 {
                     posts.length > 0
-                    ?
-                    posts.map((post, index) => 
-                    {
-                        return (
-                            <a key={ index } id={ index } onClick={ () => { handlePost(index) } }><img src={ post.file } alt="post-file" className="post-card" /></a>
-                        )
-                    })
-                    :
-                    <RingLoader color='#007BFF' />
+                        ?
+                        posts.map((post, index) => 
+                        {
+                            return (
+                                <a key={ index } id={ index } onClick={ () => { handlePost(index) } }><img src={ post.file } alt="post-file" className="post-card" /></a>
+                            )
+                        })
+                        :
+                        <RingLoader color='#007BFF' />
                 }
             </div>
 
